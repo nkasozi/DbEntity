942b4a4cf1d5c41d4afdd2e54f3ed3bb65c91411
TEST1 <TEST1@TESTSVR-NTINDA.pegasus.ug>
2018-10-13 20:55:02 +0300

    Add Test Project

---
+++ DbEntityTester/Program.cs
@@ -0,0 +1,41 @@
+﻿using DbEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbEntityTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DbResult result = DbInitializer.Initialize();
+            Item[] items3 = Item.QueryWithStoredProc("GetAllItems");
+            DbInitializer.TypesToKeepTrackOf.AddRange(new Type[] { typeof(Item) });
+            DbResult result = DbInitializer.DropAndRecreateDb();
+            //DbResult result = DbInitializer.InitializeAndUpdateSchema();
+            //result = DbInitializer.Initialize();
+            Item item = new Item
+            {
+                ItemCode = "ITEM-" + DateTime.Now.Ticks.ToString(),
+                CreatedBy = "admin",
+                ItemCount = 10,
+                ItemName = "Shoes",
+                ItemPrice = 2000,
+                ModifiedBy = "admin"
+            };
+
+            //int rowsAffected = item.SaveWithStoredProcAutoParams("SaveItem");
+            //dynamic[] all = DbEntityDbHandler.ExecuteStoredProcDynamically("GetAllItems");
+            //string itemName = all.FirstOrDefault()?.ItemCode;
+
+            Item[] items = item.QueryWithStoredProcAutoParams("GetAllItems");
+            Console.WriteLine(item.GetStoredProcedureParametersPassed());
+            //Item[] items2 = DbEntity<Item>.QueryWithStoredProc("GetAllItems");
+            //NormalDatabaseHandler dh = new NormalDatabaseHandler();
+        }
+    }
+}
---
+++ DbEntityTester/TestEntity.cs
@@ -0,0 +1,44 @@
+﻿using Castle.ActiveRecord;
+using DbEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbEntityTester
+{
+    [ActiveRecord("Items")]
+    public class Item : DbEntity<Item>
+    {
+        [PrimaryKey(PrimaryKeyType.Identity, "RecordId")]
+        public int Id { get; set; }
+
+        [Property(Length = 50)]
+        public string ItemCode { get; set; }
+
+        [Property(Length = 50)]
+        public string ItemName { get; set; }
+
+        [Property(Length = 50)]
+        public int ItemPrice { get; set; }
+
+        [Property(Length = 7500)]
+        public string ItemImage { get; set; }
+
+        [Property]
+        public int ItemCount { get; set; }
+
+        [Property(Length = 50)]
+        public string CreatedBy { get; set; }
+
+        [Property(Length = 50)]
+        public string ModifiedBy { get; set; }
+
+        [Property(Length = 50)]
+        public DateTime CreatedOn { get; set; }
+
+        [Property(Length = 50)]
+        public DateTime ModifiedOn { get; set; }
+    }
+}
